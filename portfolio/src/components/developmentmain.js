daef1ae2eaa4d6c0d07eaa9f84dd0746ca677604
Christal Vigil <c.vigil27@gmail.com>
2020-11-28 15:49:09 -0700

    updating development page

--- portfolio/src/components/developmentmain.js
+++ portfolio/src/components/developmentmain.js
@@ -43,62 +43,77 @@ function DevelopmentMain() {
           </ul>
         </div>
       </nav>
-      <div className="jumbotron">
+
+  <div className="jumbotron">
       
-        <h1>Projects</h1>
-        <p>
-            Place Images Here!
-        </p>
-      </div>
-      <div className="card">
-    </div>
+      <h1>Bill Boards</h1>
+      <ul>
+          <a href="https://sleepy-eyrie-55325.herokuapp.com/" target="_blank"><li>Deployed App</li></a>
+          <a href="https://github.com/vichrist/BillBoards" target="_blank"><li>GitHub Repository</li></a>
+      </ul>
+        {/* <img src= "../images/Project1" alt="project 1" width="1200"/> */}
+    </div><div className="card">
+  </div>
+
+
     <div className="jumbotron">
       
-      <h1>Projects</h1>
-      <p>
-          Place Images Here!
-      </p>
+        <h1>Weather Trips</h1>
+        <ul>
+            <a href="https://mmcginley32.github.io/project1/index.html" target="_blank"><li>Deployed App</li></a>
+            <a href="https://github.com/mmcginley32/project1" target="_blank"><li>GitHub Repository</li></a>
+        </ul>
+          {/* <img src= "../images/project2" alt="project 2" width="1200"/> */}
+      </div><div className="card">
     </div>
-    <div className="card">
-  </div>
-  <div className="jumbotron">
+
+    <div className="jumbotron">
       
-      <h1>Projects</h1>
-      <p>
-          Place Images Here!
-      </p>
+        <h1>Eat-Da-Burger</h1>
+        <ul>
+            <a href="https://stormy-tundra-38579.herokuapp.com/" target="_blank"><li>Deployed App</li></a>
+            <a href="https://github.com/vichrist/Add-Da-Burger-App" target="_blank"><li>GitHub Repository</li></a>
+        </ul>
+          {/* <img src= "../images/Project3" alt="project 3" width="1200"/> */}
+      </div><div className="card">
     </div>
-    <div className="card">
-  </div>
-  <div className="jumbotron">
+
+    <div className="jumbotron">
       
-      <h1>Projects</h1>
-      <p>
-          Place Images Here!
-      </p>
+        <h1>Password Generator</h1>
+        <ul>
+            <a href="https://vichrist.github.io/Password-Generator-/" target="_blank"><li>Deployed App</li></a>
+            <a href="https://github.com/vichrist/Password-Generator-" target="_blank"><li>GitHub Repository</li></a>
+        </ul>
+          {/* <img src= "../images/project4" alt="project 4" width="1200"/> */}
+      </div><div className="card">
     </div>
-    <div className="card">
-  </div>
-  <div className="jumbotron">
+
+    <div className="jumbotron">
+
+        <h1>Work Day Planner</h1>
+        <ul>
+            <a href="https://vichrist.github.io/Work-Day-Planner/" target="_blank"><li>Deployed App</li></a>
+            <a href="https://github.com/vichrist/WK-5Homework" target="_blank"><li>GitHub Repository</li></a>
+        </ul>
+          {/* <img src= "../images/Project5" alt="project 5" width="1200"/> */}
+      </div><div className="card">
+    </div>
+
+    <div className="jumbotron">
       
-      <h1>Projects</h1>
-      <p>
-          Place Images Here!
-      </p>
+        <h1>Weather Dashboard</h1>
+        <ul>
+            <a href="https://vichrist.github.io/vichrist.wk6.github.io/" target="_blank"><li>Deployed App</li></a>
+            <a href="https://github.com/vichrist/WK-6Homework" target="_blank"><li>GitHub Repository</li></a>
+        </ul>
+          {/* <img src= "../images/Project6" alt="project 6" width="1200"/> */}
+      </div><div className="card">
     </div>
-    <div className="card">
+
+
   </div>
-  <div className="jumbotron">
-      
-      <h1>Projects</h1>
-      <p>
-          Place Images Here!
-      </p>
-    </div>
-    <div className="card">
-  </div>
-    </div>
-  </div>
+</div>
   );
 }
 
