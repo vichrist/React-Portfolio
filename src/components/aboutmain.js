a2ad25abcc5e94b72f0e994414a0dbbf74c221c3
Christal Vigil <c.vigil27@gmail.com>
2020-12-01 11:46:39 -0700

    updating home page

--- src/components/aboutmain.js
+++ src/components/aboutmain.js
@@ -52,10 +52,10 @@ function AboutMain() {
       <div className="jumbotron">
       <h1 className="h1about">~ABOUT~</h1>
         <p className="p1about">
-        I'm a Colorado native raised in the central Denver area. I was 29 when I went back to school. I graduated from the Univerisity of Colorado Denver with a B.A in Economics. During my time in college I decided that I wanted to take an alternative approach for my career and get my license to sell investment products. So I obtained me Series 6 as well as a Colorado Life License and after all was said and one I still wasn't satisfied. Within a year after graduation, I decided to take on Web Development. 
+        I'm a Colorado native raised in the central Denver area. I was 29 when I went back to school. I graduated from the Univerisity of Colorado Denver with a B.A in Economics. During my time in college I decided that I wanted to take an alternative approach for my career and get licensed to sell investment products. I obtained my Series 6 as well as a Colorado Life License but after all was said and done I still wasn't satisfied. Within a year of graduation, I decided to take on Web Development. 
         </p>   
         <p className="p1about">
-        It is indeed opportunity that has allowed me to discover  my interests and passions. The idea of exerting knowledge to serve as a resource of any sort is always a reward of its own. While I have embarked a few different journeys, I have realized that my interests and passions have led me to very different paths. Finally, for the first time I have experienced a passion to do something and I look forward to building my skills and experience with full stack web development!
+        It is indeed opportunity that has allowed me to discover my interests and passions. The idea of exerting knowledge to serve as a resource of any sort is always a reward of its own. While I have embarked a few different journeys, I have realized that my interests and passions have led me to very different paths. Finally, for the first time I am experiencing a passion to do something. I look forward to continuing to build my skills and experience in full stack web development!
         </p>
         <p className="quote"><q>Sometimes in the wave of change we find our true direction.</q> <br></br>Author Unknown</p>
    
